1046a47d8dddbaea94aaea45089f6464b9759c24
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-04 22:16:10 -0600

    Add project files.

---
+++ SimpleCalculator/Module1.vb
@@ -0,0 +1,22 @@
+﻿'Elliot Heiner
+'RCET0265
+'Fall 2021
+'Simple Calculator
+
+Option Strict Off
+Option Explicit On
+Module Module1
+
+    Sub Main()
+        Dim num1 As Double
+        Dim num2 As Double
+        Console.WriteLine("Choose a number between 0 and 9.")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Choose a second number between 0 and 9")
+        num2 = Console.ReadLine()
+        Console.WriteLine(num1 & num2)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
