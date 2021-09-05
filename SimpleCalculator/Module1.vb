a8a9d04baae02d9b756b48a1112a9e64a808101d
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-05 14:07:02 -0600

    Add Double.parse after num1 and num2.

--- SimpleCalculator/Module1.vb
+++ SimpleCalculator/Module1.vb
@@ -3,8 +3,8 @@
 'Fall 2021
 'Simple Calculator
 
-Option Strict Off
-Option Explicit Off
+Option Strict On
+Option Explicit On
 Module Module1
 
     Sub Main()
@@ -12,15 +12,15 @@ Module Module1
         Dim num2 As Double
         Dim choice As String
         Console.WriteLine("This simple program will let you add or multiply two numbers of your choice. Type the first number.")
-        num1 = Console.ReadLine()
+        num1 = Double.Parse(Console.ReadLine)
         Console.WriteLine("Type the second number.")
-        num2 = Console.ReadLine()
+        num2 = Double.Parse(Console.ReadLine)
         Console.WriteLine("Type + to add or type * to multiply.")
         choice = Console.ReadLine()
         If choice = "+" Then
-            Console.WriteLine(num1 & "+" & num2 & "=" & num1 + num2)
+            Console.WriteLine(num1 & " + " & num2 & " = " & num1 + num2)
         ElseIf choice = "*" Then
-            Console.WriteLine(num1 & "*" & num2 & "=" & num1 * num2)
+            Console.WriteLine(num1 & " * " & num2 & " = " & num1 * num2)
         Else
             Console.WriteLine("Error")
         End If
