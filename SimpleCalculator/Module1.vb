dc594a577fea4106a11d873f64b069a2d9ef979a
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-04 23:39:01 -0600

    Add if then statement.

--- SimpleCalculator/Module1.vb
+++ SimpleCalculator/Module1.vb
@@ -4,18 +4,30 @@
 'Simple Calculator
 
 Option Strict Off
-Option Explicit On
+Option Explicit Off
 Module Module1
 
     Sub Main()
         Dim num1 As Double
         Dim num2 As Double
+        Dim choice As String
         Console.WriteLine("Choose a number between 0 and 9.")
         num1 = Console.ReadLine()
         Console.WriteLine("Choose a second number between 0 and 9")
         num2 = Console.ReadLine()
-        Console.WriteLine(num1 & num2)
+        Console.WriteLine("Add or multiply these numbers?")
+        choice = Console.ReadLine()
+
+        If choice = "+" Then
+            Console.WriteLine(num1 + num2)
+        ElseIf choice = "*" Then
+            Console.WriteLine(num1 * num2)
+        Else
+            Console.WriteLine("Error")
+        End If
         Console.ReadLine()
+
+
 
     End Sub
 
